1a70ae5a1cd8e84c85c02293987dc25c2ddede7c
Alex Humphreys <humphreys.a@gmail.com>
2016-05-21 17:24:09 +0200

    Add initial town schema

--- src/cljs/dd/db.cljs
+++ src/cljs/dd/db.cljs
@@ -6,3 +6,32 @@
 
 (def schema
   {:name s/Str})
+
+(def stock-schema
+  {:g s/Int
+   :w s/Int })
+
+(def price-schema
+  {:g s/Int
+   :w s/Int })
+
+(def location-schema
+  {:name s/Str
+   :demand stock-schema
+   :goons s/Int})
+
+(def transport-schema
+  {:road true
+   :rail s/Bool
+   :airport s/Bool})
+
+(def facility-schema
+  {:ware s/Keyword
+   :capacity s/Int})
+
+(def town-schema
+  {:prices price-schema
+   :stock stock-schema
+   :facilities [facility-schema]
+   :locations [location-schema]
+   :transport transport-schema})
