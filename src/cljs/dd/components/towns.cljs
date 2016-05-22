3ebaa9466a5716d3c1ac5917a8c5e02f48c74ffe
Alex Humphreys <humphreys.a@gmail.com>
2016-05-22 14:16:58 +0200

    Added tabs

--- src/cljs/dd/components/towns.cljs
+++ src/cljs/dd/components/towns.cljs
@@ -1,6 +1,7 @@
 (ns dd.components.towns
     (:require [re-frame.core :as re-frame]
               [re-com.core :as re-com]
+              [reagent.core :as reagent]
               [devtools.core :as devtools]
               ))
 
@@ -15,6 +16,35 @@
                    :label (get town :name)
                    :level :level2]]])
 
+(defn town [town]
+  [re-com/h-box
+        :gap  "1em"
+        :children [[re-com/title
+                    :label (get town :name)
+                    :level :level2]
+                   [re-com/p "Some info"]]])
+(def tabs-definition
+  [{:id ::tab1  :label "Tab1"  :say-this "I don't like my tab siblings, they smell."}
+   {:id ::tab2  :label "Tab2"  :say-this "Don't listen to Tab1, he's just jealous of my train set."}
+   {:id ::tab3  :label "Tab3"  :say-this "I'm telling Mum on you two !!"}])
+
+(defn town-tab [town]
+  (merge {:label (:name town)}
+          (select-keys town [:id])))
+
+(defn tab-styles-demo
+  []
+  (let [towns (re-frame/subscribe [:towns])
+        selected-tab-id (reagent/atom (:id (first @towns)))
+        change-tab #(reset! selected-tab-id %)]
+    (fn []
+      [re-com/h-box
+       :align    :center
+       :children [[re-com/horizontal-tabs
+                   :model     selected-tab-id
+                   :tabs      (map town-tab @towns)
+                   :on-change change-tab]]])))
+
 (defn towns-component []
   (let [towns (re-frame/subscribe [:towns])]
     (def town-ul (for [t @towns]
@@ -22,4 +52,7 @@
     (fn []
       [re-com/v-box
         :gap  "1em"
-        :children [town-ul]])))
+        :children [[tab-styles-demo]
+                   [re-com/v-box
+                     :gap  "1em"
+                     :children [town-ul]]]])))
