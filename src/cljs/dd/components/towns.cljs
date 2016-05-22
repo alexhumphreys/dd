d3469ea4027afe4a7344f02a356d248e20011525
Alex Humphreys <humphreys.a@gmail.com>
2016-05-22 13:04:40 +0200

    Add key to towns

--- src/cljs/dd/components/towns.cljs
+++ src/cljs/dd/components/towns.cljs
@@ -7,19 +7,19 @@
 (devtools/install!)
 
 (defn town-li [town]
+  ^{:key (get town :id)}
   [re-com/h-box
         :gap  "1em"
-        :children [[re-com/title
-                  :label (get town :name)
-                  :level :level1]]])
+        :children [
+                  [re-com/title
+                   :label (get town :name)
+                   :level :level2]]])
 
 (defn towns-component []
   (let [towns (re-frame/subscribe [:towns])]
-    (def town-els (for [t @towns]
-                [re-com/title
-                  :label (get t :name)
-                  :level :level1]))
+    (def town-ul (for [t @towns]
+                (town-li t)))
     (fn []
       [re-com/v-box
         :gap  "1em"
-        :children [town-els]])))
+        :children [town-ul]])))
